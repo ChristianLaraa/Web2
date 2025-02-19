431030091fcd29aa6ed312a25a5d76526fbe384a
Christian Lara <christian.lara.091204@gmail.com>
2025-02-19 12:27:47 -0600

    correcion en juego

--- 190225/app6.js
+++ 190225/app6.js
@@ -1,14 +1,16 @@
-let numeroMaquina = Math.floor(Math.random)()+(10-1);
+let numeroMaquina = Math.floor(Math.random()*(10-1)+1);
     console.log(numeroMaquina);
-let numeroUser = prompt("Introduce un número: ");
+let numeroUser =  parseInt(prompt("Ingresa un numero de 1 a 10: "));
+
 let vidas = 5;
 
-while( numeroMaquina !== numeroUser && vidas > 1){
+while( numeroMaquina !== numeroUser && vidas >1 ){
     vidas--;
-    numeroUser = parseInt(prompt("El número no es correcto. Te quedan: " + vidas));
+    numeroUser = parseInt(prompt("El numero no es correcto. Te quedan: "+vidas));
 }
-if( numeroMaquina == numeroUser){
+
+if( numeroMaquina === numeroUser){
     console.log("Ganaste");
 }else{
-    console.log("Perdiste y el numero era: " + numeroMaquina);
+    console.log("Perdiste y el numero era: "+numeroMaquina)
 }
