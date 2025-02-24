9a148259e82a5e9defa98ed3866edd2a36e4224e
Christian Lara <christian.lara.091204@gmail.com>
2025-02-24 12:38:54 -0600

    add file 240225

--- 240225/app5.js
+++ 240225/app5.js
@@ -8,7 +8,7 @@ function resta(n1, n2){
 let numero1 = prompt ("Ingresa un numero 1: ");
 let numero2 = prompt ("Ingresa un numero 2: ");
 
-console.log('La suma es: '$(suma (numero1, numero2)));
+console.log('La suma es: '(suma (numero1, numero2)));
 console.log(``)
 
 prompt ("Ingresa un numero 1: ");
