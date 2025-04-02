76412ea1198a1909af350d23f50d7556213982b2
Christian Lara <christian.lara.091204@gmail.com>
2025-04-02 12:51:43 -0600

    add new activitie 310325

---
+++ 020425/app.js
@@ -0,0 +1,6 @@
+// console.log(document);
+// console.log(document.title);
+// console.log(document.body);
+
+console.log(document.getElementById("TituloWeb"));
+console.log(document.getElementById("TituloWeb").textContent);
---
+++ 020425/informacion.php
@@ -0,0 +1,11 @@
+<?php
+    $dato1 = $_GET["Numero1"];
+    $dato2 = $_GET["Numero2"];
+    $resultado = $dato1 + $dato2;;
+
+
+
+
+
+?>
+<h1>Resultado de la suma es: <?php $resultado ?></h1>
